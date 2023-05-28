c573eef900d5126135187c3f3bfcad7a0946dcdf
themitchnz <jamesmitch@gmail.com>
2023-05-29 11:01:02 +1200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,5 @@
 let nzdf_banner, navy_banner, army_banner, air_banner;
-let nzdfColor, navyColor, armyColor, airColor;
-let service; //holds a ref to the current color scheme
-let font;
+let font, banner_buffer, current_service, current_color;
 let fontsize = 22;
 
 function preload() {
@@ -13,103 +11,34 @@ function preload() {
 }
 
 function setup() {
-    // put setup code here
-    createCanvas(1635,500);
-    //noLoop();
+    current_banner = nzdf_banner;
+    current_color = color('#ffe500');
 
-    //setup service buttons
-    text('Select Service',20,230);
+    var canvas = createCanvas(1080,122).parent('canvasforp5');
+    banner_buffer = createGraphics(1635,204).textFont(font).textSize(fontsize);
 
-    nzdfButton = createButton('nzdf');
-    nzdfButton.position(20,232);
-    nzdfButton.mousePressed(changeNzdfBanner);
-
-    navyButton = createButton('navy');
-    navyButton.position(60,232);
-    navyButton.mousePressed(changeNavyBanner);
-
-    armyButton = createButton('army');
-    armyButton.position(100,232);
-    armyButton.mousePressed(changeArmyBanner);
-
-    airButton = createButton('air');
-    airButton.position(140,232);
-    airButton.mousePressed(changeAirBanner);
+    //setup colour buttons
+    select('#nzdf-button').mousePressed(function() { current_banner = nzdf_banner; current_color = color('#ffe500'); createBanner()});
+    select('#navy-button').mousePressed(function() { current_banner = navy_banner; current_color = color('#0099d8'); createBanner()});
+    select('#army-button').mousePressed(function() { current_banner = army_banner; current_color = color('#c62026'); createBanner()});
+    select('#air-button').mousePressed(function()  { current_banner = air_banner; current_color = color('#28b6ea'); createBanner()});
 
     //setup text inputs
-    text('Course Code',20,280);
-    inputCode = createInput('Course Code');
-    inputCode.position(20,282);
-    inputCode.input(createBanner);
+    select('#inputCode').input(createBanner);
+    select('#inputName').input(createBanner);
+    select('#saveButton').mousePressed(saveImage);
 
-    text('Course Name',20,330);
-    inputName = createInput('Course Name');
-    inputName.position(20,332);
-    inputName.input(createBanner);
-
-    //setup save button
-    saveButton = createButton('Save Image');
-    saveButton.position (20,370);
-    saveButton.mousePressed(saveImage);
-
-    //set up text
-    textFont(font);
-    textSize(fontsize);
-
-    //set color schemes
-    nzdfColor = color('#ffe500');
-    navyColor = color('#0099d8');
-    armyColor = color('#c62026');
-    airColor = color('#28b6ea');
-
-    //handle params
-    let p = getURLParams();
-    service = p.service || 'nzdf';//default to NZDF to have something to start with
-    inputCode.value(p.code || 'Course Code');
-    
-    let nm = decodeURI(p.name);
-    if (nm == 'undefined') {nm = 'Course Name'};
-    inputName.value(nm);
     createBanner();
   }
   
-  function draw() {
-    // put drawing code here
-  }
-
-  //change service from button click
-function changeNzdfBanner() { service = 'nzdf'; createBanner();}
-function changeNavyBanner() { service = 'navy'; createBanner();}
-function changeArmyBanner() { service = 'army'; createBanner();}
-function changeAirBanner()  { service = 'air';  createBanner();}
-
 function createBanner () {
-    // set banner image
-    if (service == 'nzdf') {
-        image(nzdf_banner,0,0);
-        fill(nzdfColor);
-    }
-    if (service == 'navy') {
-        image(navy_banner,0,0);
-        fill(navyColor);
-    }
-    if (service == 'army') {
-        image(army_banner,0,0);
-        fill(armyColor);
-    }
-    if (service == 'air')  {
-        image(air_banner,0,0);
-        fill(airColor);
-    }
-    // set Course Code
-    text(inputCode.value(),205,152);
+    banner_buffer.background(current_banner);
+    banner_buffer.fill(current_color).text(select('#inputCode').value(),205,152);
+    banner_buffer.fill('white').text(select('#inputName').value(),205,182);
 
-    // set Course Name
-    fill('white');
-    text(inputName.value(),205,182);
+    image(banner_buffer, 0, 0, width, height, 0, 0, 1635, 204);
 }
 
 function saveImage () {
-    var im = get(0,0,1635,203);
-    im.save(inputCode.value() + '.png');
+    save(banner_buffer,select('#inputCode').value() + '.png');
 }
