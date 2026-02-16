a6fcff1e88d7b4172290c03726cae3edc2ba83c6
themitchnz <jamesmitch@gmail.com>
2026-02-17 09:41:37 +1300

    Add files via upload
    add cover image generation, tidy layout and bump lib versions

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let nzdf_banner, navy_banner, army_banner, air_banner;
+let nzdf_cover, navy_cover, army_cover, air_cover;
 let font, banner_buffer, current_service, current_color;
 let fontsize = 22;
 
@@ -7,21 +8,33 @@ function preload() {
     navy_banner = loadImage('banners/navy.png');
     army_banner = loadImage('banners/army.png');
     air_banner = loadImage('banners/air.png');
+
+    nzdf_cover = loadImage('banners/nzdf_cover.png');
+    navy_cover = loadImage('banners/navy_cover.png');
+    army_cover = loadImage('banners/army_cover.png');
+    air_cover = loadImage('banners/air_cover.png');
+
     font = loadFont('fonts/swiss.ttf');
 }
 
 function setup() {
+    // Defaults to NZDF colour scheme
     current_banner = nzdf_banner;
+    current_cover = nzdf_cover
     current_color = color('#ffe500');
 
-    var canvasW = document.getElementById('canvasContainer').offsetWidth - 25;
-    var canvas = createCanvas(canvasW,204).parent('canvasforp5');
-    banner_buffer = createGraphics(1635,204).textFont(font).textSize(fontsize);
+    //Canvas displays the preview images
+    var canvasW = document.getElementById('canvasContainer').offsetWidth - 25; //keeps it inside the container
+    var canvas = createCanvas(canvasW,400).parent('canvasforp5');
 
-        //check for URL params and update fields if there
+    //the buffers are an offpage buffer that generate the actual images - pixel perfect.
+    banner_buffer = createGraphics(1635,204).textFont(font).textSize(fontsize);
+    cover_buffer  = createGraphics(254,136).textFont(font).textSize(fontsize).textAlign(CENTER);
+
+    //check for URL params and update fields if there
     const queries = window.location.search;
     const urlParams = new URLSearchParams(queries);
-    const param_course = urlParams.get('course'); //contains whole string ie N12345  Blah Blah
+    const param_course = urlParams.get('course'); //contains whole string ie N12345 Intro to blah blah blah
 
     if (urlParams.has('course')) {
 
@@ -32,11 +45,11 @@ function setup() {
         const param_name = param_course.substring(param_course.indexOf(" ")).trimStart(); //return everything after the first space ie the course name
 
         switch(param_service) {
-            case "D": current_banner = nzdf_banner; current_color = color('#ffe500'); break;
-            case "N": current_banner = navy_banner; current_color = color('#0099d8'); break;
-            case "A": current_banner = army_banner; current_color = color('#c62026'); break;
-            case "F": current_banner = air_banner; current_color = color('#28b6ea'); break;
-            default: current_banner = nzdf_banner; current_color = color('#ffe500'); break;
+            case "D": current_banner = nzdf_banner; current_cover = nzdf_cover; current_color = color('#ffe500'); break;
+            case "N": current_banner = navy_banner; current_cover = navy_cover; current_color = color('#0099d8'); break;
+            case "A": current_banner = army_banner; current_cover = army_cover; current_color = color('#c62026'); break;
+            case "F": current_banner = air_banner;  current_cover = air_cover ; current_color = color('#28b6ea'); break;
+            default:  current_banner = nzdf_banner; current_cover = nzdf_cover; current_color = color('#ffe500'); break;
         }
 
         document.getElementById("inputCode").value = param_id;
@@ -44,27 +57,46 @@ function setup() {
     }
 
     //setup colour buttons
-    select('#nzdf-button').mousePressed(function() { current_banner = nzdf_banner; current_color = color('#ffe500'); createBanner()});
-    select('#navy-button').mousePressed(function() { current_banner = navy_banner; current_color = color('#0099d8'); createBanner()});
-    select('#army-button').mousePressed(function() { current_banner = army_banner; current_color = color('#c62026'); createBanner()});
-    select('#air-button').mousePressed(function()  { current_banner = air_banner; current_color = color('#28b6ea'); createBanner()});
+    select('#nzdf-button').mousePressed(function() { current_banner = nzdf_banner; current_cover = nzdf_cover; current_color = color('#ffe500'); createImages()});
+    select('#navy-button').mousePressed(function() { current_banner = navy_banner; current_cover = navy_cover; current_color = color('#0099d8'); createImages()});
+    select('#army-button').mousePressed(function() { current_banner = army_banner; current_cover = army_cover; current_color = color('#c62026'); createImages()});
+    select('#air-button').mousePressed(function()  { current_banner = air_banner;  current_cover = air_cover;  current_color = color('#28b6ea'); createImages()});
 
     //setup text inputs
-    select('#inputCode').input(createBanner);
-    select('#inputName').input(createBanner);
-    select('#saveButton').mousePressed(saveImage);
+    select('#inputCode').input(createImages);
+    select('#inputName').input(createImages);
+    select('#saveImages').mousePressed(saveImages);
 
-    createBanner();
-  }
-  
-function createBanner () {
+    createImages();
+}
+
+function createImages () {
+    //create the banner image in the buffer
     banner_buffer.background(current_banner);
     banner_buffer.fill(current_color).text(select('#inputCode').value(),205,152);
     banner_buffer.fill('white').text(select('#inputName').value(),205,182);
 
+    //display a preview of the banner image on screen
     image(banner_buffer, 0, 0, 1635, 204, 0, 0, 1635, 204);
+
+    //create the cover image in the buffer
+    cover_buffer.background(current_cover);
+    cover_buffer.fill(current_color).text(select('#inputCode').value(),127,60);
+    cover_buffer.fill('white').text(select('#inputName').value(),0,78,254).textSize(18).textLeading(20).textWrap(WORD);
+    
+    //display a preview of the cover image on screen
+    image(cover_buffer, 0, 220)
 }
 
-function saveImage () {
-    save(banner_buffer,select('#inputCode').value() + '.png');
+//saves the images created in the buffers - browser may not like saving two files at once 
+function saveImages () {
+    saveBanner(); saveCover();
+}
+
+function saveBanner () {
+    save(banner_buffer,select('#inputCode').value() + '_banner' + '.png');
+}
+
+function saveCover () {
+    save(cover_buffer,select('#inputCode').value() + '_cover' + '.png');
 }
